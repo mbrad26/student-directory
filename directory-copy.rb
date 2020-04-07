3a66d74add13fc05e5378f5e3a91ac671e82098b
mbrad26 <mbrad26@gmail.com>
2020-04-07 11:51:17 +0000

    more refactoring

--- directory-copy.rb
+++ directory-copy.rb
@@ -1,15 +1,15 @@
-def input_students
+def input_students 
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name and month
-  name = gets.chomp
-  month = gets.chomp
-  country_of_birth = gets.chomp
+  name = gets.rstrip 
+  month = gets.rstrip
+  country_of_birth = gets.chomp 
   # while the name is not empty, repeat this code
   while !name.empty? && !month.empty? do
-    students << { name: name, cohort: month, country: country_of_birth }
+    students << {name: name, cohort: month.to_sym, country: country_of_birth.to_sym }
     if students.length == 1
       puts "Now we have #{students.count} student"
     else
@@ -37,17 +37,17 @@ def print_header
 end
 
 def print_names(names)
+=begin
   count = 0
   while count < names.size
     puts "#{count + 1}. #{names[count][:name].ljust(8)} (#{names[count][:cohort]} cohort).center(15), #{names[count][:country].center(4)}"
     count += 1
   end
-  
-=begin
+=end 
+
   names.each_with_index do |name, index| 
-    puts "#{index + 1}. #{name[:name].ljust(8)} (#{name[:cohort]} cohort).center(15), #{name[:country].center(4)}" # if name[:name].length < 12 # if name[:name].start_with?("A")
+    puts "#{index + 1}. #{name[:name].ljust(12)}" + "(#{name[:cohort].to_s} cohort)".center(20) + "#{name[:country].to_s.center(6)}" # if name[:name].length < 12 # if name[:name].start_with?("A")
   end
-=end
 end
 
 def print_footer(names)
