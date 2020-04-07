df786c510ec667c7dba15fdc6de1a2dea10be9ed
mbrad26 <mbrad26@gmail.com>
2020-04-07 07:44:04 +0000

    Refactored print_names and input_students

--- directory-copy.rb
+++ directory-copy.rb
@@ -6,9 +6,10 @@ def input_students
   # get the first name and month
   name = gets.chomp
   month = gets.chomp
+  country_of_birth = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? && !month.empty? do
-    students << { name: name, cohort: month.to_sym }
+    students << { name: name, cohort: month, country: country_of_birth }
     if students.length == 1
       puts "Now we have #{students.count} student"
     else
@@ -17,6 +18,7 @@ def input_students
     # get another name, month from the user
     name = gets.chomp
     month = gets.chomp
+    country_of_birth = gets.chomp
   end
   # return the array of students
   students
@@ -26,9 +28,7 @@ def print_by_cohort(names)
   puts "What cohort do you like to print?"
   # get the month and use it to print students names by cohort
   month = gets.chomp
-  names.map do |name|
-    puts name[:name] if name[:cohort] == month.to_sym
-  end
+  names.each {|name| puts name[:name] if name[:cohort] == month}
 end
 
 def print_header
@@ -37,9 +37,17 @@ def print_header
 end
 
 def print_names(names)
-  names.each_with_index do |name, index| 
-    puts "#{index + 1}. #{name[:name]} (#{name[:cohort]} cohort)" if name[:name].length < 12 # if name[:name].start_with?("A")
+  count = 0
+  while count < names.size
+    puts "#{count + 1}. #{names[count][:name].ljust(8)} (#{names[count][:cohort]} cohort).center(15), #{names[count][:country].center(4)}"
+    count += 1
   end
+  
+=begin
+  names.each_with_index do |name, index| 
+    puts "#{index + 1}. #{name[:name].ljust(8)} (#{name[:cohort]} cohort).center(15), #{name[:country].center(4)}" # if name[:name].length < 12 # if name[:name].start_with?("A")
+  end
+=end
 end
 
 def print_footer(names)
